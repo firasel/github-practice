d3586cbf73005ae1823fc8557d1e7a652d7568a3
md:rasel <mdrasel601390@gmail.com>
2019-10-23 05:14:33 +0600

    we have added a new code.

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ var full_name = function(first_name, last_name) {
     console.log(first_name + last_name);
 };
 full_name(first_name, last_name);
+
+var total_cost = () => {
+    var first = 159;
+    var second = 147;
+    var third = 1489;
+    var total = first + second + third;
+    return total;
+}
+total_cost();
