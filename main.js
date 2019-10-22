712b5b839f1c5376483f16a18d2379e04a46b429
md:rasel <mdrasel601390@gmail.com>
2019-10-23 05:06:29 +0600

    we have added a new line

---
+++ main.js
@@ -0,0 +1,6 @@
+var first_name = "md";
+var last_name = " rasel.";
+var full_name = function(first_name, last_name) {
+    console.log(first_name + last_name);
+};
+full_name(first_name, last_name);
